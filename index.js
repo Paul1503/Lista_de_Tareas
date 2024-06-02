70eafdf2d5327992232bdb81d346b06345249be7
Paul1503 <paul.arellano.93@gmail.com>
2024-06-02 18:01:28 -0400

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+//Funcion que nos permite agregar una tarea
+function agregar_tarea(){
+    //Capturamos el nombre de la tarea.
+    let nombre_tarea = document.getElementById('input_nombre').value;
+    console.log(nombre_tarea);
+
+
+}
